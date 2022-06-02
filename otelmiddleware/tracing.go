73cb16badded28e8c728e21f58d83e652d0ca95a
Vincent Free <vincent.free@ing.com>
2022-06-02 12:31:59 +0200

    Add documentation

--- otelmiddleware/tracing.go
+++ otelmiddleware/tracing.go
@@ -12,78 +12,104 @@ import (
 
 const version = "0.0.1"
 
+// TraceOption takes a traceConfig struct and applies changes.
 type TraceOption func(*traceConfig)
 
+// traceConfig contains all of the configuration for the library.
 type traceConfig struct {
 	serviceName string
 	tracer      trace.Tracer
 	propagator  propagation.TextMapPropagator
 }
 
+// TraceWithOptions takes TraceOption's and initializes a new trace.Span.
 func TraceWithOptions(opt ...TraceOption) func(next http.Handler) http.Handler {
+	// initialize an empty traceConfig.
 	config := &traceConfig{}
+
+	// apply the configuration passed to the function.
 	for _, o := range opt {
 		o(config)
 	}
+	// check for the traceConfig.tracer if absent use a default value.
 	if config.tracer == nil {
 		config.tracer = otel.Tracer("otel-tracer", trace.WithInstrumentationVersion(version))
 	}
+	// check for the traceConfig.propagator if absent use a default value.
 	if config.propagator == nil {
 		config.propagator = otel.GetTextMapPropagator()
 	}
+	// check for the traceConfig.serviceName if absent use a default value.
 	if config.serviceName == "" {
 		config.serviceName = "TracedApplication"
 	}
+	// the handler that initializes the trace.Span.
 	return func(next http.Handler) http.Handler {
+
+		// assign the handler which creates the OpenTelemetry trace.Span.
 		fn := func(w http.ResponseWriter, r *http.Request) {
 			requestCtx := r.Context()
+			// extract the OpenTelemetry span context from the context.Context object.
 			ctx := config.propagator.Extract(requestCtx, propagation.HeaderCarrier(r.Header))
+			// the standard trace.SpanStartOption options whom are applied to every server handler.
 			opts := []trace.SpanStartOption{
 				trace.WithAttributes(semconv.NetAttributesFromHTTPRequest("tcp", r)...),
 				trace.WithAttributes(semconv.EndUserAttributesFromHTTPRequest(r)...),
 				trace.WithAttributes(semconv.HTTPServerAttributesFromHTTPRequest(r.Host, extractRoute(r.RequestURI), r)...),
 				trace.WithSpanKind(trace.SpanKindServer),
 			}
+			// extract the route name which is used for setting a usable name of the span.
 			spanName := extractRoute(r.RequestURI)
 			if spanName == "" {
 				spanName = fmt.Sprintf("HTTP %s route not found", r.Method)
 			}
+
+			// create a good name to recognize where the span originated.
 			spanName = fmt.Sprintf("%s /%s", r.Method, spanName)
+
+			// start the actual trace.Span.
 			ctx, span := config.tracer.Start(ctx, spanName, opts...)
+
 			defer span.End()
 
-			// pass the span through the request context
+			// pass the span through the request context.
 			r = r.WithContext(ctx)
 
-			// serve the request to the next middleware
+			// serve the request to the next middleware.
 			next.ServeHTTP(w, r)
 		}
+
 		return http.HandlerFunc(fn)
 	}
 }
 
+// Trace uses the TraceWithOptions without additional options, this is a shorthand for TraceWithOptions().
 func Trace(next http.Handler) http.Handler {
 	return TraceWithOptions()(next)
 }
 
+// extract the route name
 func extractRoute(uri string) string {
 	return uri[1:]
 }
 
+// WithTracer is a TraceOption to inject your own trace.Tracer.
 func WithTracer(tracer trace.Tracer) TraceOption {
 	return func(c *traceConfig) {
 		c.tracer = tracer
 	}
 }
 
+// WithPropagator is a TraceOption to inject your own propagation.
 func WithPropagator(p propagation.TextMapPropagator) TraceOption {
 	return func(c *traceConfig) {
 		c.propagator = p
 	}
 }
 
-func WithServiceName(service string) TraceOption {
+// WithServiceName is a TraceOption to inject your own serviceName.
+func WithServiceName(serviceName string) TraceOption {
 	return func(c *traceConfig) {
-		c.serviceName = service
+		c.serviceName = serviceName
 	}
 }
