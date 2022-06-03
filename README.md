3238bddf6801450b80095994eec7c43c240ea198
Vincent Free <vincent.free@ing.com>
2022-06-03 15:56:57 +0200

    update docs, add badges to README.md

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
-# opentelemetry-http
+# OpenTelemetry http
+
+[![Go](https://github.com/vincentfree/opentelemetry-http/actions/workflows/go.yml/badge.svg)](https://github.com/vincentfree/opentelemetry-http/actions/workflows/go.yml)
+[![CodeQL](https://github.com/vincentfree/opentelemetry-http/actions/workflows/codeql.yml/badge.svg)](https://github.com/vincentfree/opentelemetry-http/actions/workflows/codeql.yml)
+[![Dependency Review](https://github.com/vincentfree/opentelemetry-http/actions/workflows/dependency-review.yml/badge.svg)](https://github.com/vincentfree/opentelemetry-http/actions/workflows/dependency-review.yml)
+[![Go Reference](https://pkg.go.dev/badge/github.com/vincentfree/opentelemetry-http/otelmiddleware.svg)](https://pkg.go.dev/github.com/vincentfree/opentelemetry-http/otelmiddleware)
 
 Open Telemetry http middleware. This package provides an instrumentation for middleware that can be used to trace HTTP requests.
 
 ## version
 
-`0.0.4`
+`0.0.5`
--- otelmiddleware/doc.go
+++ otelmiddleware/doc.go
@@ -7,11 +7,13 @@ the basic information is extracted using the OpenTelemetry semconv package.
 When a span gets initialized it uses the following slice of trace.SpanStartOption
 
 	opts := []trace.SpanStartOption{
-				trace.WithAttributes(semconv.NetAttributesFromHTTPRequest("tcp", request)...),
-				trace.WithAttributes(semconv.EndUserAttributesFromHTTPRequest(request)...),
-				trace.WithAttributes(semconv.HTTPServerAttributesFromHTTPRequest(request.Host, extractRoute(request.RequestURI), request)...),
-				trace.WithSpanKind(trace.SpanKindServer),
+		trace.WithAttributes(semconv.NetAttributesFromHTTPRequest("tcp", request)...),
+		trace.WithAttributes(semconv.EndUserAttributesFromHTTPRequest(request)...),
+		trace.WithAttributes(semconv.HTTPServerAttributesFromHTTPRequest(request.Host, extractRoute(request.RequestURI), request)...),
+		trace.WithSpanKind(trace.SpanKindServer),
 	}
+
+The slice can be extended using the WithAttributes TraceOption function.
 
 After these options are applied a new span is created and the middleware will pass the http.ResponseWriter and http.Request to the next http.Handler.
 
@@ -33,17 +35,5 @@ Structs
 		attributes []attribute.KeyValue
 		serviceName string
 	}
-
-Examples
-	// create a new ServeMux
-	serve := http.NewServeMux()
-	// add a new route to the ServeMux
-	serve.Handle("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello World"))
-	}))
-	// create the Trace middleware and decorate the ServeMux routes with this middleware.
-	handler := TraceWithOptions(WithServiceName("ExampleService"))(serve)
-	http.ListenAndServe(":8080", handler)
-
 */
 package otelmiddleware
