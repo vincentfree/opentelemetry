f535f2c6d6f608071c8e5debee95e4a368f5e683
Vincent Free <vincent.free@ing.com>
2023-07-21 13:38:33 +0200

    update otelmiddleware, fix empty path issue
    Signed-off-by: Vincent Free <vincent.free@ing.com>

--- otelmiddleware/doc.go
+++ otelmiddleware/doc.go
@@ -1,3 +1,17 @@
+// Copyright 2023 Vincent Free
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 /*
 Package otelmiddleware provides middleware for wrapping http.Server handlers with Open Telemetry tracing support.
 
@@ -18,6 +32,7 @@ The slice can be extended using the WithAttributes TraceOption function.
 After these options are applied a new span is created and the middleware will pass the http.ResponseWriter and http.Request to the next http.Handler.
 
 Functions
+
 	func TraceWithOptions(opt ...TraceOption) func(next http.Handler) http.Handler
 	func Trace(next http.Handler) http.Handler
 	func WithAttributes(attributes ...attribute.KeyValue) TraceOption
@@ -26,9 +41,11 @@ Functions
 	func WithTracer(tracer trace.Tracer) TraceOption
 
 Types
+
 	type TraceOption func(*traceConfig)
 
 Structs
+
 	type traceConfig struct {
 		tracer trace.Tracer
 		propagator propagation.TextMapPropagator
@@ -36,4 +53,4 @@ Structs
 		serviceName string
 	}
 */
-package otelmiddleware
+package otelmiddleware // import "github.com/vincentfree/opentelemetry/otelmiddleware"
--- otelmiddleware/tracing.go
+++ otelmiddleware/tracing.go
@@ -1,7 +1,20 @@
+// Copyright 2023 Vincent Free
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package otelmiddleware
 
 import (
-	"fmt"
 	"net/http"
 
 	"go.opentelemetry.io/otel"
@@ -12,7 +25,7 @@ import (
 )
 
 // version is used as the instrumentation version.
-const version = "0.0.7"
+const version = "0.0.8"
 
 // TraceOption takes a traceConfig struct and applies changes.
 // It can be passed to the TraceWithOptions function to configure a traceConfig struct.
@@ -72,11 +85,12 @@ func TraceWithOptions(opt ...TraceOption) func(next http.Handler) http.Handler {
 			// extract the route name which is used for setting a usable name of the span.
 			spanName := extractRoute(r.RequestURI)
 			if spanName == "" {
-				spanName = fmt.Sprintf("HTTP %s route not found", r.Method)
+				// no path available
+				spanName = "HTTP " + r.Method + " /"
 			}
 
 			// create a good name to recognize where the span originated.
-			spanName = fmt.Sprintf("%s /%s", r.Method, spanName)
+			spanName = r.Method + " /" + spanName
 
 			// start the actual trace.Span.
 			ctx, span := config.tracer.Start(ctx, spanName, opts...)
@@ -85,6 +99,8 @@ func TraceWithOptions(opt ...TraceOption) func(next http.Handler) http.Handler {
 
 			// pass the span through the request context.
 			r = r.WithContext(ctx)
+			carrier := propagation.HeaderCarrier(r.Header)
+			otel.GetTextMapPropagator().Inject(ctx, carrier)
 
 			// serve the request to the next middleware.
 			next.ServeHTTP(w, r)
