d213928f55ea151deab82cf3ea0efe8235fa859b
Vincent Free <vincent.free@ing.com>
2023-08-05 00:10:56 +0200

    Add open telemetry logging with benchmarking tests
    This commit adds open telemetry implementations for logging to the project. Three different logging frameworks have been used: zerolog, slog, and logrus
    
    Signed-off-by: Vincent Free <vincent.free@ing.com>

---
+++ cmd/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"context"
+	"github.com/rs/zerolog/log"
+
+	// "github.com/sirupsen/logrus"
+	// "github.com/vincentfree/opentelemetry/otellogrus"
+	"github.com/vincentfree/opentelemetry/otelslog"
+	"github.com/vincentfree/opentelemetry/otelzerolog"
+	"go.opentelemetry.io/otel"
+	"golang.org/x/exp/slog"
+)
+
+func main() {
+	ctx := context.Background()
+	_, span := otel.Tracer("test").Start(context.Background(), "mainService")
+	// zerolog implementation
+	log.Info().Func(otelzerolog.AddTracingContext(span)).Msg("logging with zerolog")
+
+	// slog implementation
+	sLogger := otelslog.New()
+	sLogger.WithTracingContext(ctx, slog.LevelInfo, "logging with slog", span, nil)
+
+	// logrus implementation
+	// lrLogger := otellogrus.New(otellogrus.WithLevel(logrus.InfoLevel), otellogrus.WithFormatter(&logrus.JSONFormatter{}))
+
+}
