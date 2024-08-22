ea60d1ef4f64925c66519b050a918abfbcf70250
Vincent Free <vincent.free@ing.com>
2024-08-22 14:19:53 +0200

    Refactor provider configuration and add licensing
    Refactored provider configuration to use a new Provider interface, replacing the existing OtelConfig structure. Added Apache 2.0 license headers to all files, ensuring proper usage and distribution compliance.
    
    Signed-off-by: Vincent Free <vincent.free@ing.com>

--- providerconfig/doc.go
+++ providerconfig/doc.go
@@ -13,33 +13,3 @@
 // limitations under the License.
 
 package providerconfig
-
-import "slices"
-
-type Execution uint8
-
-//sync async
-
-const (
-	Sync Execution = iota + 1
-	Async
-)
-
-var (
-	executionTypes = []Execution{Sync, Async}
-)
-
-func (e Execution) String() string {
-	switch e {
-	case Sync:
-		return "Sync"
-	case Async:
-		return "Async"
-	default:
-		return "undefined"
-	}
-}
-
-func (e Execution) IsValid() bool {
-	return slices.Contains(executionTypes, e)
-}
