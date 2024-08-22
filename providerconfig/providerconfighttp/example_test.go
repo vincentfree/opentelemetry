ba1c6594af787de45b7a4b251786f254735d2d7f
Vincent Free <vincent.free@ing.com>
2024-08-22 14:20:34 +0200

    Add Apache 2.0 license headers to all source files
    Each source file now includes Apache 2.0 license headers to ensure compliance and clarify licensing terms. This change affects multiple files across different packages.
    
    Signed-off-by: Vincent Free <vincent.free@ing.com>

--- providerconfig/providerconfighttp/example_test.go
+++ providerconfig/providerconfighttp/example_test.go
@@ -1,3 +1,17 @@
+// Copyright 2024 Vincent Free
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package providerconfighttp_test
 
 import (
@@ -15,17 +29,17 @@ func ExampleNew() {
 	signalProcessor := providerconfighttp.New(
 		providerconfighttp.WithCollectorEndpoint("0.0.0.0", 9898),
 	)
-	otelConfig := providerconfig.New(providerconfig.WithApplicationName("example-app"),
+	provider := providerconfig.New(providerconfig.WithApplicationName("example-app"),
 		providerconfig.WithApplicationVersion("0.1.0"),
 		providerconfig.WithSignalProcessor(signalProcessor),
 	)
 	// set providers
-	otel.SetTracerProvider(otelConfig.TraceProvider())
-	otel.SetMeterProvider(otelConfig.MetricProvider())
-	global.SetLoggerProvider(otelConfig.LogProvider())
+	otel.SetTracerProvider(provider.TraceProvider())
+	otel.SetMeterProvider(provider.MetricProvider())
+	global.SetLoggerProvider(provider.LogProvider())
 
 	// shutdown all providers
-	otelConfig.ShutdownAll()
+	provider.ShutdownAll()
 }
 
 func ExampleWithCollectorEndpoint() {
